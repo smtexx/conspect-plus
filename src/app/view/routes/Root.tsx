3635b6e996e1f30de9a9e2186acd53ac949fd152
Roman Kolmakov <smtexx@hotmail.com>
2023-07-21 13:10:23 +0200

    feat: add bs base structure and Footer

--- src/app/view/routes/Root.tsx
+++ src/app/view/routes/Root.tsx
@@ -1,15 +1,22 @@
-import { Container, Row, Col } from 'react-bootstrap';
+import Footer from '../../../components/Footer/Footer';
 import NavBar from '../../../components/NavBar/NavBar';
 
 export default function Root() {
   return (
-    <>
-      <NavBar />
-      <Container>
-        <Row>
-          <Col></Col>
-        </Row>
-      </Container>
-    </>
+    <div className="container vh-100 d-flex flex-column">
+      <div className="row">
+        <NavBar />
+      </div>
+      <div className="row flex-grow-1">
+        <div className="col">
+          <main></main>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <Footer />
+        </div>
+      </div>
+    </div>
   );
 }
