7c508c3be4223eb1d0ac5b77b0c83958f9db804f
Roman Kolmakov <smtexx@hotmail.com>
2023-07-21 13:09:53 +0200

    init: create footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,20 @@
+export default function Footer() {
+  return (
+    <footer>
+      <div className="row bg-body-tertiary px-2 py-3 px-lg-3 py-lg-2">
+        <div className="col d-flex align-items-center">
+          <address className="m-0 text-center">
+            Связь с разработчиком:{' '}
+            <a
+              className="text-white"
+              href="mailto:smtexx@hotmail.com"
+              rel="noreferrer noopener"
+            >
+              smtexx@hotmail.com
+            </a>
+          </address>
+        </div>
+      </div>
+    </footer>
+  );
+}
