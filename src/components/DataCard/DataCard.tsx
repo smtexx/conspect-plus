5cde0e1de177ac1c85b7292fc9a249bf640e2305
Roman Kolmakov <smtexx@hotmail.com>
2023-07-27 18:04:12 +0200

    styles: fix height of title and description blocks

--- src/components/DataCard/DataCard.tsx
+++ src/components/DataCard/DataCard.tsx
@@ -15,14 +15,18 @@ export default function DataCard({
   saved,
 }: I_Props) {
   return (
-    <Link className="cm-card-link w-100" to={id}>
-      <Card>
+    <Link className="cm-card-link d-block" to={id}>
+      <Card style={{ width: '20rem' }}>
         <Card.Body>
-          <Card.Title>{title}</Card.Title>
+          <Card.Title style={{ height: '2.5em', overflow: 'hidden' }}>
+            {title}
+          </Card.Title>
           <Card.Subtitle className="mb-2 text-muted">
             {saved.toLocaleString()}
           </Card.Subtitle>
-          <Card.Text>{description}</Card.Text>
+          <Card.Text style={{ height: '4.25em', overflow: 'hidden' }}>
+            {description}
+          </Card.Text>
         </Card.Body>
       </Card>
     </Link>
