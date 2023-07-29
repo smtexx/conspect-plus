1000f86025f9e38b3ca3f9d13859ce0651c0e540
Roman Kolmakov <smtexx@hotmail.com>
2023-07-29 11:36:44 +0200

    Extract form to create new conspect into separate component

--- src/components/Conspect/Conspect.tsx
+++ src/components/Conspect/Conspect.tsx
@@ -1,54 +1,18 @@
-import { Button, FloatingLabel, Form } from 'react-bootstrap';
-import { useState, ChangeEvent } from 'react';
+import { useState } from 'react';
 import Screen from '../Screen/Screen';
 import { fakeConspects } from '../../fakeData/getFakeConspects';
 import DataCard from '../DataCard/DataCard';
 import CustomSideMenu from '../CustomSideMenu/CustomSideMenu';
+import CreateConspectForm from '../CreateConspectForm/CreateConspectForm';
 
 export default function Conspect() {
   const [menuIsOpen, setMenuIsOpen] = useState(false);
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
-  const [createDisabled, setCreateDisabled] = useState(true);
-
-  const conspectRules = {
-    titleLength: [3, 20],
-    descriptionLength: [10, 40],
-  };
 
   function handleMenuOpen() {
     setMenuIsOpen(true);
   }
   function handleMenuClose() {
-    setTitle('');
-    setDescription('');
-    setCreateDisabled(true);
     setMenuIsOpen(false);
-  }
-
-  function unlockCreate() {
-    if (
-      title.length >= conspectRules.titleLength[0] &&
-      title.length <= conspectRules.titleLength[1] &&
-      description.length >= conspectRules.descriptionLength[0] &&
-      description.length <= conspectRules.descriptionLength[1]
-    ) {
-      return setCreateDisabled(false);
-    } else {
-      return setCreateDisabled(true);
-    }
-  }
-
-  function handleTitleChange(e: ChangeEvent<HTMLInputElement>) {
-    const value = e.target.value.trim();
-    setTitle(value);
-    unlockCreate();
-  }
-
-  function handleDescriptionChange(e: ChangeEvent<HTMLInputElement>) {
-    const value = e.target.value.trim();
-    setDescription(value);
-    unlockCreate();
   }
 
   const { conspects } = fakeConspects;
@@ -71,48 +35,7 @@ export default function Conspect() {
       </Screen>
 
       <CustomSideMenu show={menuIsOpen} onHide={handleMenuClose}>
-        <Form>
-          <h5>Создать конспект</h5>
-          <p>
-            Для создания нового конспекта заполните поля с названием и
-            описанием конспекта:
-          </p>
-          <FloatingLabel
-            controlId="conspectTitleInput"
-            label="Название конспекта"
-          >
-            <Form.Control
-              type="text"
-              placeholder="Название конспекта"
-              value={title}
-              onChange={handleTitleChange}
-              aria-labelledby="titleHelpBlock"
-            />
-          </FloatingLabel>
-          <p id="titleHelpBlock" className="form-text">
-            {`Длинна заголовка от ${conspectRules.titleLength[0]} до ${conspectRules.titleLength[1]} символов.`}
-          </p>
-          <FloatingLabel
-            controlId="conspectDescriptionInput"
-            label="Описание конспекта"
-          >
-            <Form.Control
-              as="textarea"
-              placeholder="Описание конспекта"
-              style={{ height: '100px' }}
-              value={description}
-              onChange={handleDescriptionChange}
-            />
-          </FloatingLabel>
-          <p id="descriptionHelpBlock" className="form-text">
-            {`Длинна описания от ${conspectRules.descriptionLength[0]} до ${conspectRules.descriptionLength[1]} символов.`}
-          </p>
-          <div className="pt-3 d-flex justify-content-end">
-            <Button variant="primary" disabled={createDisabled}>
-              Создать
-            </Button>
-          </div>
-        </Form>
+        <CreateConspectForm />
       </CustomSideMenu>
     </>
   );
---
+++ src/components/CreateConspectForm/CreateConspectForm.tsx
@@ -0,0 +1,84 @@
+import { Button, FloatingLabel } from 'react-bootstrap';
+import { Form } from 'react-bootstrap';
+import { useState, ChangeEvent } from 'react';
+
+export default function CreateConspectForm() {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [createDisabled, setCreateDisabled] = useState(true);
+
+  const conspectRules = {
+    titleLength: [3, 20],
+    descriptionLength: [10, 40],
+  };
+
+  function unlockCreate() {
+    if (
+      title.length >= conspectRules.titleLength[0] &&
+      title.length <= conspectRules.titleLength[1] &&
+      description.length >= conspectRules.descriptionLength[0] &&
+      description.length <= conspectRules.descriptionLength[1]
+    ) {
+      return setCreateDisabled(false);
+    } else {
+      return setCreateDisabled(true);
+    }
+  }
+
+  function handleTitleChange(e: ChangeEvent<HTMLInputElement>) {
+    const value = e.target.value.trim();
+    setTitle(value);
+    unlockCreate();
+  }
+
+  function handleDescriptionChange(e: ChangeEvent<HTMLInputElement>) {
+    const value = e.target.value.trim();
+    setDescription(value);
+    unlockCreate();
+  }
+
+  return (
+    <Form>
+      <h5>Создать конспект</h5>
+      <p>
+        Для создания нового конспекта заполните поля с названием и
+        описанием конспекта:
+      </p>
+      <FloatingLabel
+        controlId="conspectTitleInput"
+        label="Название конспекта"
+      >
+        <Form.Control
+          type="text"
+          placeholder="Название конспекта"
+          value={title}
+          onChange={handleTitleChange}
+          aria-labelledby="titleHelpBlock"
+        />
+      </FloatingLabel>
+      <p id="titleHelpBlock" className="form-text">
+        {`Длинна заголовка от ${conspectRules.titleLength[0]} до ${conspectRules.titleLength[1]} символов.`}
+      </p>
+      <FloatingLabel
+        controlId="conspectDescriptionInput"
+        label="Описание конспекта"
+      >
+        <Form.Control
+          as="textarea"
+          placeholder="Описание конспекта"
+          style={{ height: '100px' }}
+          value={description}
+          onChange={handleDescriptionChange}
+        />
+      </FloatingLabel>
+      <p id="descriptionHelpBlock" className="form-text">
+        {`Длинна описания от ${conspectRules.descriptionLength[0]} до ${conspectRules.descriptionLength[1]} символов.`}
+      </p>
+      <div className="pt-3 d-flex justify-content-end">
+        <Button variant="primary" disabled={createDisabled}>
+          Создать
+        </Button>
+      </div>
+    </Form>
+  );
+}
