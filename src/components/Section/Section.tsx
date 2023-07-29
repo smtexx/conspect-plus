bb27b022b662c04a97215e4bc3488890cab51b8b
Roman Kolmakov <smtexx@hotmail.com>
2023-07-29 11:29:36 +0200

    Add CustomSideMenu

--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -1,13 +1,26 @@
 import { fakeConspects } from '../../fakeData/getFakeConspects';
+import { useState } from 'react';
 import Screen from '../Screen/Screen';
 import ResourceLink from '../ResourceLink/ResourceLink';
+import CustomSideMenu from '../CustomSideMenu/CustomSideMenu';
 
 export default function Section() {
-  const conspect = fakeConspects.conspects[2];
+  const [optionsIsOpen, setOptionsIsOpen] = useState(false);
 
+  function handleOptionsOpen() {
+    setOptionsIsOpen(true);
+  }
+  function handleOptionsClose() {
+    setOptionsIsOpen(false);
+  }
+
+  const conspect = fakeConspects.conspects[2];
   return (
     <>
-      <Screen title={conspect.title} optionsHandler={() => {}}>
+      <Screen
+        title={conspect.title}
+        optionsHandler={handleOptionsOpen}
+      >
         <ul className="list-unstyled cm-links-list">
           {conspect.sections.map((section, idx) => (
             <li key={section.id}>
@@ -20,6 +33,13 @@ export default function Section() {
           ))}
         </ul>
       </Screen>
+
+      <CustomSideMenu
+        show={optionsIsOpen}
+        onHide={handleOptionsClose}
+      >
+        Hello
+      </CustomSideMenu>
     </>
   );
 }
