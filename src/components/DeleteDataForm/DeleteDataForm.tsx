e3026d2011a44fc4a8486b8c91eab8c3119ba077
Roman Kolmakov <smtexx@hotmail.com>
2023-07-29 12:38:48 +0200

    Create component

---
+++ src/components/DeleteDataForm/DeleteDataForm.tsx
@@ -0,0 +1,56 @@
+import { Button } from 'react-bootstrap';
+import CustomSideForm from '../CustomSideForm/CustomSideForm';
+import ConfirmModal from '../ConfirmModal/ConfirmModal';
+import { useState } from 'react';
+
+interface I_Props {
+  title: string;
+  description: string;
+  confirmTitle: string;
+  confirmText: string;
+  deleteHandler: () => void;
+}
+
+export default function DeleteDataForm({
+  title,
+  description,
+  confirmText,
+  confirmTitle,
+  deleteHandler,
+}: I_Props) {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  function handleModalClose() {
+    setModalIsOpen(false);
+  }
+  function handleModalIsOpen() {
+    setModalIsOpen(true);
+  }
+  function handleModalConfirm() {
+    deleteHandler();
+    handleModalClose();
+  }
+
+  return (
+    <>
+      <CustomSideForm title={title} description={description}>
+        <div className="pt-3 pb-5 d-flex justify-content-end">
+          <Button variant="primary" onClick={handleModalIsOpen}>
+            Удалить
+          </Button>
+        </div>
+      </CustomSideForm>
+
+      <ConfirmModal
+        title={confirmTitle}
+        open={modalIsOpen}
+        disabled={false}
+        onHide={handleModalClose}
+        buttonText="Удалить"
+        buttonHandler={handleModalConfirm}
+      >
+        <p>{confirmText}</p>
+      </ConfirmModal>
+    </>
+  );
+}
