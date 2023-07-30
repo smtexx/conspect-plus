eb1e80fcc9303810c9934a0a44e2aecd448c49de
Roman Kolmakov <smtexx@hotmail.com>
2023-07-30 09:14:30 +0200

    Do not wrap inside content into CustomSideForm

--- src/components/CreateSectionForm/CreateSectionForm.tsx
+++ src/components/CreateSectionForm/CreateSectionForm.tsx
@@ -1,13 +1,12 @@
 import { Button, FloatingLabel, Form } from 'react-bootstrap';
 import { useState, ChangeEvent } from 'react';
-import CustomSideForm from '../CustomSideForm/CustomSideForm';
 
 export default function CreateSectionForm() {
   const [title, setTitle] = useState('');
   const [createDisabled, setCreateDisabled] = useState(true);
 
   const conspectRules = {
-    titleLength: [3, 50],
+    titleLength: [3, 30],
   };
 
   function unlockCreate() {
@@ -28,10 +27,7 @@ export default function CreateSectionForm() {
   }
 
   return (
-    <CustomSideForm
-      title="Создать новый раздел"
-      description="Для создания нового раздела в текущем коспекте заполните поле ниже:"
-    >
+    <>
       <FloatingLabel
         controlId="conspectTitleInput"
         label="Название раздела"
@@ -48,11 +44,11 @@ export default function CreateSectionForm() {
         {`Длинна названия от ${conspectRules.titleLength[0]} до ${conspectRules.titleLength[1]} символов.`}
       </p>
 
-      <div className="pt-3 pb-5 d-flex justify-content-end">
+      <div className="pt-3 d-flex justify-content-end">
         <Button variant="primary" disabled={createDisabled}>
           Сохранить
         </Button>
       </div>
-    </CustomSideForm>
+    </>
   );
 }
