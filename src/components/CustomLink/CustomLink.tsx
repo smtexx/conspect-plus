72cc29d356f493bbfa46ee81fc3ca8fef3e4f300
Roman Kolmakov <smtexx@hotmail.com>
2023-08-06 21:44:31 +0200

    Create component

---
+++ src/components/CustomLink/CustomLink.tsx
@@ -0,0 +1,55 @@
+import { Link } from 'react-router-dom';
+import { FaExternalLinkAlt } from 'react-icons/fa';
+import s from './CustomLink.module.scss';
+
+interface I_PropsExternal {
+  href: string;
+  text: string;
+  counter?: number;
+  external: true;
+  date?: Date;
+}
+
+interface I_PropsInternal {
+  href: string;
+  text: string;
+  counter: number;
+  external?: false;
+  date?: Date;
+}
+
+export default function CustomLink({
+  href,
+  text,
+  counter,
+  date,
+  external,
+}: I_PropsExternal | I_PropsInternal) {
+  const parts = (
+    <>
+      <span className={s.icon}>
+        {external ? <FaExternalLinkAlt /> : `#${counter}`}
+      </span>
+      <span className={s.text}>{text}</span>
+      {date && (
+        <span className={s.date}>
+          {` [${date.toLocaleString()}]`}
+        </span>
+      )}
+    </>
+  );
+  return external ? (
+    <a
+      className={s.external}
+      href={href}
+      rel="noreferrer noopener"
+      target="_blank"
+    >
+      {parts}
+    </a>
+  ) : (
+    <Link className={s.internal} to={href}>
+      {parts}
+    </Link>
+  );
+}
