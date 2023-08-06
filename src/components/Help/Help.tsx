a9553258e275b7248977850e9413be5ec8956f3f
Roman Kolmakov <smtexx@hotmail.com>
2023-08-06 21:47:43 +0200

    Create route and component

---
+++ src/components/Help/Help.tsx
@@ -0,0 +1,11 @@
+import Screen from '../Screen/Screen';
+
+export default function Help() {
+  return (
+    <>
+      <Screen title="Инструкция">
+        <p>Инструкция находится в процессе разработки :)</p>
+      </Screen>
+    </>
+  );
+}
---
+++ src/components/Search/Search.tsx
@@ -0,0 +1,11 @@
+import Screen from '../Screen/Screen';
+
+export default function Search() {
+  return (
+    <>
+      <Screen title="Поиск">
+        <p>Функция поиска находится в процессе разработки :)</p>
+      </Screen>
+    </>
+  );
+}
