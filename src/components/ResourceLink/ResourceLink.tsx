51ae1c2053abf9edc290f8192943eff955575d6c
Roman Kolmakov <smtexx@hotmail.com>
2023-08-06 13:46:34 +0200

    Add date feature

--- src/components/ResourceLink/ResourceLink.tsx
+++ src/components/ResourceLink/ResourceLink.tsx
@@ -5,13 +5,22 @@ interface I_Props {
   to: string;
   text: string;
   counter: number;
+  date?: Date;
 }
 
-export default function ResourceLink({ to, text, counter }: I_Props) {
+export default function ResourceLink({
+  to,
+  text,
+  counter,
+  date,
+}: I_Props) {
   return (
     <Link to={to} className={s.wrapper}>
       <span className={s.marker}>{`#${counter + 1}`}</span>
       <span className={s.text}>{text}</span>
+      {date && (
+        <span className={s.date}>{`[${date.toLocaleString()}]`}</span>
+      )}
     </Link>
   );
 }
