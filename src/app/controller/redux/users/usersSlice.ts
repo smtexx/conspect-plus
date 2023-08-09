2d78864e57269e4d3c54d7f94ca311a5afe2da67
Roman Kolmakov <smtexx@hotmail.com>
2023-08-09 16:56:56 +0200

    Create usersSlice

---
+++ src/app/controller/redux/users/usersSlice.ts
@@ -0,0 +1,36 @@
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
+import { RootState } from '../store';
+import { I_User } from '../../../model/typesModel';
+
+export const usersSlice = createSlice({
+  name: 'users',
+  initialState: [] as I_User[],
+  reducers: {
+    createUser: (state, action: PayloadAction<string>) => {
+      state.push({
+        login: action.payload,
+        isActive: false,
+        created: new Date(),
+        lastActivity: new Date(),
+        notes: 0,
+      });
+    },
+    setUserActive: (state, action: PayloadAction<string>) => {
+      const user = state.find(
+        (user) => user.login === action.payload
+      );
+      if (user) {
+        state.forEach((user) => (user.isActive = false));
+        user.isActive = true;
+      }
+    },
+  },
+});
+
+export const { createUser, setUserActive } = usersSlice.actions;
+export const { reducer: usersReducer } = usersSlice;
+
+// Selectors
+export const selectUsers = (state: RootState) => state.users;
+export const selectActiveUser = (state: RootState) =>
+  state.users.find((user) => user.isActive);
