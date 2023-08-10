097ab3f43fe16a28f8ca5b93297c9501fc8dff18
Roman Kolmakov <smtexx@hotmail.com>
2023-08-10 18:05:41 +0200

    Connect dataReducer to store

--- src/app/controller/redux/store.ts
+++ src/app/controller/redux/store.ts
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { usersReducer } from './users/usersSlice';
+import { dataReducer } from './data/dataSlice';
 
 export const store = configureStore({
-  reducer: { users: usersReducer },
+  reducer: { users: usersReducer, data: dataReducer },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
